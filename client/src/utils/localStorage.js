0b103c6f5d6aff4fb9b5c8cae8cc230dc71f7267
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-08-16 21:54:41 -0700

    fix import of third-party icon

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,30 +1,30 @@
 export const getSavedPoiIds = () => {
-    const savedPoiIds = localStorage.getItem('saved_pois')
-      ? JSON.parse(localStorage.getItem('saved_pois'))
-      : [];
-  
-    return savedPoiIds;
-  };
-  
-  export const savePoiIds = (poiIdArr) => {
-    if (poiIdArr.length) {
-      localStorage.setItem('saved_pois', JSON.stringify(poiIdArr));
-    } else {
-      localStorage.removeItem('saved_pois');
-    }
-  };
-  
-  export const removePoiId = (poiId) => {
-    const savedPoiIds = localStorage.getItem('saved_pois')
-      ? JSON.parse(localStorage.getItem('saved_pois'))
-      : null;
-  
-    if (!savedPoiIds) {
-      return false;
-    }
-  
-    const updatedSavedPoiIds = savedPoiIds?.filter((savedPoiId) => savedPoiId !== poiId);
-    localStorage.setItem('saved_pois', JSON.stringify(updatedSavedPoiIds));
-  
-    return true;
-  };
+  const savedPoiIds = localStorage.getItem('saved_pois')
+    ? JSON.parse(localStorage.getItem('saved_pois'))
+    : [];
+
+  return savedPoiIds;
+};
+
+export const savePoiIds = (poiIdArr) => {
+  if (poiIdArr.length) {
+    localStorage.setItem('saved_pois', JSON.stringify(poiIdArr));
+  } else {
+    localStorage.removeItem('saved_pois');
+  }
+};
+
+export const removePoiId = (poiId) => {
+  const savedPoiIds = localStorage.getItem('saved_pois')
+    ? JSON.parse(localStorage.getItem('saved_pois'))
+    : null;
+
+  if (!savedPoiIds) {
+    return false;
+  }
+
+  const updatedSavedPoiIds = savedPoiIds?.filter((savedPoiId) => savedPoiId !== poiId);
+  localStorage.setItem('saved_pois', JSON.stringify(updatedSavedPoiIds));
+
+  return true;
+};
