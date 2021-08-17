a82915400094579632a49b94c04918f99675c130
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-08-16 17:22:00 -0700

    define user-related queries

---
+++ client/src/utils/queries.js
@@ -0,0 +1,13 @@
+// import dependency
+import { gql } from '@apollo/client';
+
+// define query
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+    }
+  }
+`;
