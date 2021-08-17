5865e0c0a71d36de290b330f79dad2fcaadff8b1
Rod Blanco <rodrigo.a.blanco1990@gmail.com>
2021-08-16 20:19:18 -0700

    started adding schema logic

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,17 +1,29 @@
+// import dependency
 const { gql } = require('apollo-server-express');
 
+// define object types
 const typeDefs = gql`
-type POI {
+  type Query {
+    me: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+   
+  }
+  type User {
+    _id: ID
+    username: String!
+    email: String!
+    password: String!
+  }
 
-}
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-type User{
-
-}
-
-
-
+  }
 `;
-
 
 module.exports = typeDefs;
