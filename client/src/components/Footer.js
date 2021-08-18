5b9f079bbbb9cae9e56198e90d5e01fcd3658724
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-08-17 23:18:32 -0700

    add footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,41 @@
+// import react dependencies 
+import {
+  Box,
+  IconButton,
+  Container,
+  Stack,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { VscGithub } from "react-icons/vsc";
+
+const Footer = () => {
+  return (
+    <Box
+      bg={useColorModeValue('gray.50', 'gray.900')}
+      color={useColorModeValue('gray.700', 'gray.200')}>
+      <Container
+        as={Stack}
+        maxW={'6xl'}
+        py={4}
+        direction={{ base: 'column', md: 'row' }}
+        spacing={4}
+        justify={{ base: 'center', md: 'space-between' }}
+        align={{ base: 'center', md: 'center' }}>
+        <Text>© 2021 Trip Assist. All rights reserved</Text>
+        <Stack direction={'row'} spacing={6}>
+          <IconButton
+            variant="outline"
+            colorScheme="teal"
+            aria-label="GitHub"
+            fontSize="20px"
+            icon={<VscGithub />}
+            // onClick={}
+          />
+        </Stack>
+      </Container>
+    </Box>
+  );
+}
+
+export default Footer;
