be63de650819ba2944b653ac6c2ce48ec512f74a
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-08-19 20:11:34 -0700

    convert LoginForm.js component to Login.js page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,127 @@
+// import react dependencies
+import React, { useState } from 'react';
+import { BrowserRouter, Link as ReactLink } from 'react-router-dom';
+
+// import chakra dependencies
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Link,
+  Stack,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { EmailIcon, LockIcon } from '@chakra-ui/icons'
+
+// import apollo dependency
+import { useMutation } from '@apollo/client';
+
+// import utils dependencies
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+
+const LoginForm = () => {
+  const [formData, setFormData] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
+
+  // handle form submit
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const mutationResponse = await login({
+        variables: { email: formData.email, password: formData.password },
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // handle changes to input field
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  return (
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'}>Sign In</Heading>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={3}>
+            <form onSubmit={handleFormSubmit}>
+              <FormControl id="email" isRequired>
+                <FormLabel>E-mail</FormLabel>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<EmailIcon color="gray.300" />}
+                  />
+                  <Input
+                    type="email"
+                    placeholder='E-mail'
+                    name='email'
+                    onChange={handleChange}
+                    value={formData.email}
+                  />
+                </InputGroup>
+              </FormControl>
+              <FormControl id="password" isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<LockIcon color="gray.300" />}
+                  />
+                  <Input
+                    type="password"
+                    placeholder='Password'
+                    name='password'
+                    onChange={handleChange}
+                    value={formData.password}
+                  />
+                </InputGroup>
+              </FormControl>
+              <Stack spacing={3}>
+                <Button
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{ bg: 'blue.500' }}
+                >
+                  Sign In
+                </Button>
+                <Stack align={'start'}>
+                  <BrowserRouter>
+                    <Link as={ReactLink} to={'/signup'} color={'blue.400'}>Sign Up</Link>
+                  </BrowserRouter>
+                </Stack>
+              </Stack>
+            </form>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+};
+
+export default LoginForm;
