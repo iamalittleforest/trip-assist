fb6e72f33e1567b816918656215ae6f4978d5275
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-08-21 13:55:11 -0700

    integrate API functionality within file

--- client/src/pages/SearchPOIs.js
+++ client/src/pages/SearchPOIs.js
@@ -1,5 +1,5 @@
-// import dependencies
-import React, { useState, useEffect } from "react";
+// import react dependencies
+import React, { useState, useEffect } from 'react';
 import {
   Jumbotron,
   Container,
@@ -8,96 +8,132 @@ import {
   Button,
   Card,
   CardColumns,
-} from "react-bootstrap";
-import axios from "axios";
-import { useMutation } from "@apollo/client";
-import { SAVE_POI } from "../utils/mutations";
-import {} from "../utils/API";
-// need to import saved POI logic when ready
-import {} from "../utils/localStorage";
-import Auth from "../utils/auth";
+} from 'react-bootstrap';
 
-//API Key
-const key = "AIzaSyAGMm8VfMoIWD35Z0G0dNAldoEokk20Vow";
+// import apollo dependency
+import { useMutation } from '@apollo/client';
+
+// import axios dependency
+import axios from 'axios';
+
+// import utils dependencies
+import { SAVE_POI } from '../utils/mutations';
+import { savePOIIds, getSavedPOIIds } from '../utils/localStorage';
+import Auth from '../utils/auth';
+
+// API Key
+const key = 'AIzaSyAGMm8VfMoIWD35Z0G0dNAldoEokk20Vow';
+
 // API urls
-const baseURL1 =
-  "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?&inputtype=textquery&fields=formatted_address,name,rating,opening_hours,geometry";
-const baseURL2 =
-  "https://maps.googleapis.com/maps/api/place/nearbysearch/json?&radius=1500&type=tourist_attraction&key=AIzaSyAGMm8VfMoIWD35Z0G0dNAldoEokk20Vow";
-const baseURL3 =
-  "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&key=AIzaSyAGMm8VfMoIWD35Z0G0dNAldoEokk20Vow";
+const getLocation =
+  'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?&inputtype=textquery&fields=formatted_address,name,rating,opening_hours,geometry';
+const getPOIs =
+  'https://maps.googleapis.com/maps/api/place/nearbysearch/json?&radius=1500&type=tourist_attraction';
+const getImgURL =
+  'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400';
 
-const SearchPOI = () => {
+const SearchPOIs = () => {
+
   // create state for holding returned google api data
   const [searchedPOIs, setSearchedPOIs] = useState([]);
 
-  // create state for holding our search field data
-  const [searchInput, setSearchInput] = useState("");
+  // create state for holding input data
+  const [searchInput, setSearchInput] = useState('');
 
   // create state to hold saved POI Id values
-  const [savedPOIds, setSavedPOIds] = useState(getSavedPOIIds());
+  const [savedPOIIds, setSavedPOIIds] = useState([]);
 
-  const [attractions, setAttractions] = useState([]);
+  // 
+  const [POIs, setPOIs] = useState([]);
 
-  const findAttractions = (lonlat) => {
-    axios.get(baseURL2 + "&location=" + lonlat).then(({ data }) => {
-      console.log("Raw data", data);
-      getImgs(
-        data.results.map((a) => a.photos?.[0].photo_reference),
-        data.results
-      );
-    });
-  };
-  const getImgs = async (refs, attractions) => {
-    const urls = refs.map((ref) =>
-      ref
-        ? baseURL3 + "&photo_reference=" + ref
-        : "https://cdn.shopify.com/s/files/1/0054/4371/5170/products/FiGPiN_360HelloKittySANRIOPIN.png?v=1627413934"
-    );
-    setAttractions(
-      attractions.map((a, i) => {
-        a.url = urls[i];
-        return a;
-      })
-    );
-  };
   // set up useEffect hook to save `savedPoiIds` list to localStorage on component unmount
-  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
     return () => savePOIIds(savedPOIIds);
   });
 
-  // set mutation for saving Poi
+  // set mutation for saving POI
   const [savePOI] = useMutation(SAVE_POI);
 
   // create method to search for POIs and set state on form submit
-  const handleFormSubmit = () => {
-    axios
-      .get(`${baseURL1}&key=${key}&input=${searchInput}`)
-      .then((response) => {
-        console.log("BaseURl1", response.data.candidates);
-        const { lng, lat } = response.data.candidates[0].geometry.location;
-        findAttractions(`${lat},${lng}`);
-      });
-    const searchedData = items
-      .map((results) => ({
-        POI_id: results.place_id,
-        name: results.name,
-        img: results.photos,
-        business_status: results.business_status,
-        rating: results.rating,
-        vicinity: results.vicinity,
-      }))
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
 
-      .catch((error) => {
-        console.log(error);
-      });
+    if (!searchInput) {
+      return false;
+    }
+
+    // use cached data if it exists
+    if (localStorage.getItem(searchInput.toLowerCase().trim())) {
+      console.log("using cached data ---");
+      return setPOIs(JSON.parse(localStorage.getItem(searchInput.toLowerCase().trim())));
+    }
+
+    // get data from API
+    try {
+      console.log("searching using API --- ")
+      axios
+        .get(`${getLocation}&key=${key}&input=${searchInput}`)
+        .then((res) => {
+          console.log('getLocation', res.data.candidates);
+          const { lat, lng } = res.data.candidates[0].geometry.location;
+          findPOIs(`${lat},${lng}`);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+      const findPOIs = (latlng) => {
+        axios
+          .get(`${getPOIs}&key=${key}&location=${latlng}`)
+          .then(({ data }) => {
+            console.log('getPOIs', data);
+            getImgs(
+              data.results.map((a) => a.photos?.[0].photo_reference),
+              data.results
+            );
+          });
+      };
+
+      const getImgs = async (refs, POIs) => {
+        const urls = refs.map((ref) =>
+          ref
+            ? `${getImgURL}&key=${key}&photo_reference=${ref}`
+            : 'https://www.eduprizeschools.net/wp-content/uploads/2016/06/No_Image_Available.jpg'
+        );
+        const POI_Data =
+          POIs.map((a, i) => {
+            a.POI_id = a.place_id;
+            a.url = urls[i];
+            a.img = a.url;
+            return a;
+          });
+        //store in state also store in localstorage for caching search results
+        setPOIs(POI_Data);
+        localStorage.setItem(searchInput.toLowerCase().trim(), JSON.stringify(POI_Data))
+      };
+
+      // const { POIs } = await response.json();
+
+      // const POIData = POIs.map((POI) => ({
+      //   POI_id: POI.POI_id,
+      //   name: POI.name,
+      //   img: POI.photos,
+      //   business_status: POI.business_status,
+      //   rating: POI.rating,
+      //   vicinity: POI.vicinity,
+      // }));
+
+      // setSearchedPOIs(POIData);
+      // setSearchInput('');
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   // create function to handle saving a POI to our database
-  const handleSavePOI = async (POIId) => {
-    // find the poi in `searchedPois` state by the matching id
-    const POIToSave = searchedPOIs.find((POI) => POI.POIId === POIId);
+  const handleSavePOI = async (POI_id) => {
+    // find the poi in `searchedPOIs` state by the matching id
+    const POIToSave = searchedPOIs.find((POI) => POI.POI_id === POI_id);
 
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -108,43 +144,41 @@ const SearchPOI = () => {
 
     // save POI
     try {
-      const response = await savevPOI({
+      const res = await savePOI({
         variables: { POIToSave },
       });
 
-      if (!response.data) {
-        throw new Error("something went wrong!");
+      if (!res.data) {
+        throw new Error('something went wrong!');
       }
 
-      // if poi successfully saves to user's account, save POI id to state
-      setSavedPOIIds([...savedPOIIds, POIToSave.POIId]);
+      // if POI successfully saves to user's account, save POI id to state
+      setSavedPOIIds([...savedPOIIds, POIToSave.POI_id]);
     } catch (err) {
-      console.error(err);
+      console.err(err);
     }
   };
 
   return (
     <>
-      <Jumbotron fluid className="text-light bg-dark">
+      <Jumbotron fluid className='text-light bg-dark'>
         <Container>
-          {/* Need a better title */}
-          <h1>Search for your places of Interest!</h1>
+          <h1>Search for places to go!</h1>
           <Form onSubmit={handleFormSubmit}>
             <Form.Row>
               <Col xs={12} md={8}>
                 <Form.Control
-                  name="searchInput"
+                  name='searchInput'
                   value={searchInput}
                   onChange={(e) => setSearchInput(e.target.value)}
-                  type="text"
-                  size="lg"
-                  //   might need to change placholder
-                  placeholder="Search for places to travel"
+                  type='text'
+                  size='lg'
+                  placeholder='Enter City Name'
                 />
               </Col>
               <Col xs={12} md={4}>
-                <Button type="submit" variant="success" size="lg">
-                  Submit Search
+                <Button type='submit' variant='success' size='lg'>
+                  Search
                 </Button>
               </Col>
             </Form.Row>
@@ -154,38 +188,42 @@ const SearchPOI = () => {
 
       <Container>
         <h2>
-          {searchedPOIs.length
-            ? `Viewing ${searchedPOIs.length} results:`
-            : "Search for a POI to begin"}
+          {POIs.length
+            ? `Viewing ${POIs.length} results:`
+            : 'Search for a POI to begin'}
         </h2>
         <CardColumns>
-          {searchedPOIs.map((POI) => {
+          {POIs.map((POI) => {
             return (
-              <Card key={POI.POIId} border="dark">
+              <Card key={POI.POI_id} border='dark'>
                 {POI.img ? (
                   <Card.Img
                     src={POI.img}
-                    alt={`The cover for ${POI.title}`}
-                    variant="top"
+                    alt={`The cover for ${POI.name}`}
+                    variant='top'
                   />
                 ) : null}
                 <Card.Body>
-                  <Card.Title>{POI.title}</Card.Title>
-                  <p className="small">Authors: {POI.authors}</p>
-                  <Card.Text>{POI.description}</Card.Text>
+                  <Card.Title>{POI.name}</Card.Title>
+                  <Card.Text>
+                    Status: {POI.business_status}
+                  </Card.Text>
+                  <Card.Text>
+                    Rating: {POI.rating}
+                  </Card.Text>
                   {Auth.loggedIn() && (
                     <Button
                       disabled={savedPOIIds?.some(
-                        (savedPOIId) => savedPOIId === POI.POIId
+                        (savedPOIId) => savedPOIId === POI.POI_id
                       )}
-                      className="btn-block btn-info"
-                      onClick={() => handleSavePOI(POI.POIId)}
+                      className='btn-block btn-info'
+                      onClick={() => handleSavePOI(POI.POI_id)}
                     >
                       {savedPOIIds?.some(
-                        (savedPOIId) => savedPOIId === POI.POISId
+                        (savedPOIId) => savedPOIId === POI.POI_id
                       )
-                        ? "This point of interest has already been saved!"
-                        : "Save this point of interest!"}
+                        ? 'This point of interest has already been saved!'
+                        : 'Save this point of interest!'}
                     </Button>
                   )}
                 </Card.Body>
