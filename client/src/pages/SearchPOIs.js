4603ff6ae63363e34b4a700af19f8586ceb4a2dc
Rod Blanco <rodrigo.a.blanco1990@gmail.com>
2021-08-27 20:10:29 -0700

    fixed cors error

--- client/src/pages/SearchPOIs.js
+++ client/src/pages/SearchPOIs.js
@@ -31,11 +31,11 @@ import POICard from '../components/POICard';
 
 // API urls
 const getLocation =
-  'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?&inputtype=textquery&fields=formatted_address,name,rating,opening_hours,geometry';
+  "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?&inputtype=textquery&fields=formatted_address,name,rating,opening_hours,geometry";
 const getPOIs =
-  'https://maps.googleapis.com/maps/api/place/nearbysearch/json?&radius=1500&type=tourist_attraction';
+  "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?&radius=1500&type=tourist_attraction";
 const getImgURL =
-  'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400';
+  "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400";
 
 // search for POIs
 const SearchPOIs = () => {
@@ -92,13 +92,14 @@ const SearchPOIs = () => {
             "Access-Control-Allow-Origin": "*",
           },
         })
+       
         .then((res) => {
           console.log("getLocation", res.data.candidates);
           const { lat, lng } = res.data.candidates[0].geometry.location;
           findPOIs(`${lat},${lng}`);
         })
         .catch((err) => {
-          console.log(err);
+          console.log("is ths the error", err);
         });
 
       // use location to get nearby POIs
